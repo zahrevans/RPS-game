97882a605468c63b9626c9ce98d6b20f9203eaa1
zahrevans <526zevans@gmail.com>
2025-01-14 09:30:57 -0500

    testlayout

---
+++ newscript.js
@@ -0,0 +1,55 @@
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+        return "The game is a tie!";
+    }
+    if (userChoice === "rock") {
+        if (computerChoice === "paper") {
+            return "Computer won.";
+        } else {
+            return "You won!";
+        }
+    }
+
+    if (userChoice === "paper") {
+        if (computerChoice === "scissors") {
+            return "Computer won.";
+        } else {
+            return "You won!";
+        }
+    }
+
+    if (userChoice === "scissors") {
+        if (computerChoice === "rock") {
+            return "Computer won.";
+        } else {
+            return "You won!";
+        }
+    }
+
+    if (userChoice === "bomb") {
+        return "You won!";
+    }
+};
+
+const playGame = (userChoice) => {
+
+    const computerChoice = getComputerChoice();
+    console.log(`You chose: ${userChoice}.`);
+    console.log(`Computer chose: ${computerChoice}.`);
+
+    console.log(determineWinner(userChoice, computerChoice));
+};
+
+
